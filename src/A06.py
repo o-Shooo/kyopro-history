e3ee32ad5f16fab375b93fb3a9d2b2f3795beae5
Sho <o.shogo.1021q@gmail.com>
2025-04-09 23:08:23 +0900

    A06

--- src/A06.py
+++ src/A06.py
@@ -1,19 +1,10 @@
-n, q = map(int, input().split(" "))
-a = list(map(int, input().split(" ")))
+N, Q = map(int, input().split())
+A = list(map(int, input().split()))
 
-s = []
-for i in range(n):
-    if i == 0:
-        s.append(a[i])
-    else:
-        s.append(s[i - 1] + a[i])
+W = [0] * (N + 1)
+for i in range(N):
+    W[i + 1] = W[i] + A[i]
 
-result = []
-for _ in range(q):
-    l, r = map(int, input().split(" "))
-    if l == 1:
-        result.append(s[r - 1])
-    else:
-        result.append(s[r - 1] - s[l - 1 - 1])
-
-print(*result, sep="\n")
+for i in range(Q):
+    L, R = map(int, input().split())
+    print(W[R] - W[L - 1])
